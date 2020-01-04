111f15cbb3cc445e59f8353642f8c37769111c60
blazej laszewski <lasicb@gmail.com>
2020-01-04 11:53:08 +0100

    use express static

--- src/app.js
+++ src/app.js
@@ -1,13 +1,12 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const { X11Class } = require('addon-x11');
 
 const X11Obj = new X11Class();
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function(socket){
   console.log('a user connected');
